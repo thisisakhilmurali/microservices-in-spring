df7c71228e8765ad1984b4344c8b63364b0770c8
akhil <akhilmuraliai21@gmail.com>
2023-04-06 20:13:20 +0530

    Config Server Created

---
+++ CloudConfigServer/src/main/java/com/ust/cloudconfigserver/CloudConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.ust.cloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CloudConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigServerApplication.class, args);
+    }
+
+}
